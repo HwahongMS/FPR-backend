e0eaa87fe424875382f5055640ff32e9f8a4a1a4
UU_jeong <80961726+oU-Ua@users.noreply.github.com>
2024-01-06 17:05:55 +0900

    Initial Setting

---
+++ src/test/java/com/fpr/FprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
